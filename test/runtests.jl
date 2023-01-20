5b7350941a4283135e15bfe530714c3708a4b8af
anand jain <anandj@uchicago.edu>
2023-01-20 21:46:40 +0000

    add channels in ci

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,9 @@ using Test
 
 # run(pipeline(`curl -fsSL https://install.julialang.org`, `sh -s -- -y`))
 # run(`juliaup config channelsymlinks true`)
-
+run(`source /home/runner/.bashrc`)
+run(`source /home/runner/.profile`)
+run(`source /home/runner/.bash_profile`)
 
 pkgs = ["Tables", "OrdinaryDiffEq", "DifferentialEquations", "ModelingToolkit", "Plots"]
 pkgs = ["CSV", "DataFrames", "Plots"]
@@ -12,7 +14,7 @@ channels = "1." .* string.(6:9)
 # seems like this loop fails w @async
 # for c in channels[1:end-1] # dont do 1.9 cuz it fucks up my env
 for c in channels # on ci, i dont care
-    run(`juliaup add $c`)
+    # run(`juliaup add $c`)
     for pkg in pkgs
         JuliaBenchmarker.doit(c, pkg)
     end
